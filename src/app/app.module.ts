18b176e37a5b3090ddbd75c7af8e2dc03894d10c
Riyasheri <Riyariyaaddon2022@gmail.com>
2022-07-07 23:51:03 +0530

    viewgame

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddgamesComponent } from './addgames/addgames.component';
+import { ViewgameComponent } from './viewgame/viewgame.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddgamesComponent
+    AddgamesComponent,
+    ViewgameComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/viewgame/viewgame.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewgame',
+  templateUrl: './viewgame.component.html',
+  styleUrls: ['./viewgame.component.css']
+})
+export class ViewgameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
